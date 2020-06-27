df7d448801d420ddb2638e2127156e787800e6a8
Abdelrahman Kinani <a.kinani@arco.sa>
2020-06-27 16:55:27 +0200

    Add project files.

---
+++ DiagonalDifference.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomProblemSolving
+{
+    // https://www.hackerrank.com/challenges/diagonal-difference
+    public static class DiagonalDifference
+    {
+        public static int diagonalDifference(List<List<int>> arr)
+        {
+            var firstDiagonalSum = 0;
+            var secondDiagonalsum = 0;
+
+            for (var (rowIndexStart, rowIndexEnd) = (0, arr.Count - 1);
+                rowIndexStart < arr.Count; rowIndexStart++, rowIndexEnd--)
+            {
+                firstDiagonalSum += arr[rowIndexStart][rowIndexStart];
+                secondDiagonalsum += arr[rowIndexStart][rowIndexEnd];
+            }
+
+            return Math.Abs(firstDiagonalSum - secondDiagonalsum);
+        }
+    }
+}
